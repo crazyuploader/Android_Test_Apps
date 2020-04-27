255b2712a6b1646c72786b5a2ebde405239eb51d
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-04-27 21:01:40 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-curl -s https://raw.githubusercontent.com/crazyuploader/Bash/master/build_droid.sh | bash -
+curl -sO https://raw.githubusercontent.com/crazyuploader/Bash/master/build_droid.sh
+bash build_droid.sh
