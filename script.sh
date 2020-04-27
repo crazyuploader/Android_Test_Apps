c89a99d4e9cc4a889a5ffa69f171739e4a182556
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-04-27 23:20:41 +0530

    Better handling of error if any generated by previous script
    script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,8 @@
 
 curl -sO https://raw.githubusercontent.com/crazyuploader/Bash/master/build_droid.sh
 bash build_droid.sh
+ERROR_CODE="$?"
 rm build_droid.sh
+if [[ "${ERROR_CODE}" != "0" ]]; then
+    exit 1
+fi
